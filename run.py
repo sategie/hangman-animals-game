6020756051dd2b1ed4730612c134894f1bfa1b3d
sategie <>
2023-06-01 15:50:47 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Import random module to select random words
+import random
+
+print("HANGMAN: Animals Version")
+
