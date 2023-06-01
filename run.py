4f73558cb0fad6957fcbb2d4b3137c767ba24563
sategie <>
2023-06-01 16:17:51 +0000

    Create a list of words for the game and show introductory message when the name of the user is entered

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 #Import random module to select random words
 import random
+#Import time  module to create a delay at certain points
+import time
+#Import list of words from words module
+from words import word_list
 
 print("HANGMAN: Animals Version")
+
+name = input("Please enter your name: ")
+print("loading...")
+time.sleep(2)
+print(f"Welcome to the Hangman Animals game, {name}!\n")
+
+print(word_list)
+
 
