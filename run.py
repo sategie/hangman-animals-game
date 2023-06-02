a1955e9baa84b0fdc4c12c74231e19296385b312
sategie <>
2023-06-02 13:55:46 +0000

    Create initial functions to print the welcome message and get user's name

--- run.py
+++ run.py
@@ -6,18 +6,35 @@ import time
 from words import word_list
 
 print("HANGMAN: Animals Version")
+print("........................")
 
-name = input("Please enter your name: ")
-print("loading...")
-time.sleep(1)
-print(f"Welcome to the Hangman Animals game, {name}!\n")
+def print_welcome(name):
+    """
+    Function to display a welcome message after the user enters a name
+    """
+    print(f"Welcome to the Hangman Animals game, {name}!\n")
 
-def random_word():
+def print_loading():
+    """
+    Function to display the loading sign before the game starts
+    """
+    print("loading...")
+    time.sleep(1)
+
+def random_word(word_list):
     """
     This gets a random word from the list of words
     """
     word = random.choice(word_list)
     return word
 
-random_word()
+def play_hangman(name):
+    print_loading()
+    print_welcome(name)
+
+#Check if module is run as the main program and run the function
+if __name__ == "__main__":
+  name = input("Please enter your name: ")
+  play_hangman(name)
+
 
